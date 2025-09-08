b67220a993eab1816b053c752cf8ea17d4ed6014
Jack Pan <xinjackpan@gmail.com>
2025-09-08 09:56:07 +0800

    'random' string fix + dimension tables

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -4,7 +4,6 @@ import argparse, os, pathlib, random
 from datetime import datetime, timedelta, date
 import numpy as np
 from faker import Faker
-from faker_commerce import Provider
 from mimesis import Person, Address
 import rstr
 import pyarrow as pa
@@ -13,6 +12,15 @@ import xlsxwriter
 
 #for commerce synthetic data
 from faker_commerce import Provider
+
+
+# random character/ digit fix
+
+import rstr
+import string
+
+# Define the correct character set
+charset = string.ascii_uppercase + string.digits 
 
 def parse_args():
     ap = argparse.ArgumentParser()
@@ -34,7 +42,7 @@ def main():
     with customers_path.open('w', encoding='utf-8') as f:
         f.write('customer_id,natural_key,first_name,last_name,email,phone,address_line1,address_line2,city,state_region,postcode,country_code,latitude,longitude,birth_date,join_ts,is_vip,gdpr_consent\n')
         for i in range(1, 80000):  # TODO raise to 80_000
-            nk = 'CUST-' + rstr.rstr('A-Z0-9', 8)
+            nk = 'CUST-' + rstr.rstr(charset, 8)
             email = fake.email() if random.random()>0.1 else 'bad_email'
             lat = -44 + random.random()*10; lon = 112 + random.random()*40
             birth = date(1960,1,1) + timedelta(days=random.randint(0, 20000))
@@ -45,7 +53,7 @@ def main():
     with products_path.open('w', encoding='utf-8') as f:
         f.write('product_id,sku,name,category,subcategory,current_price,currency,introduced_dt,discontinued_dt,is_discontinued\n')
         for i in range(1, 25000):  
-            nk = 'SKU-' + rstr.rstr('A-Z0-9', 6)
+            nk = 'SKU-' + rstr.rstr(charset, 6)
             current_price = round(random.uniform(1, 10000), 4)# Random price (DECIMAL 12,4 style)
             introduced_dt = date.today() - timedelta(days=random.randint(0, 2000))
             # Determine discontinued date and indicator
@@ -57,6 +65,42 @@ def main():
                 discontinued_dt = date(9999, 12, 31)
                 is_discontinued = False
             f.write(f"{i},{nk},{fake.ecommerce_name()},{fake.ecommerce_category()},{fake.ecommerce_material()},{current_price},{fake.currency_code()},{introduced_dt},{discontinued_dt},{is_discontinued}\n")
+    #Stores
+        stores_path = out/'stores.csv'
+    with stores_path.open('w', encoding='utf-8') as f:
+        f.write('store_id,store_code,name,channel,region,state,latitude,longitude,open_dt,close_dt\n')
+        for i in range(1, 5000):  
+            
+            if random.random() < 0.5: # 50/50 online or retail
+                channel = 'online'
+                nk =  'O-' + rstr.rstr(charset, 4)
+            else:
+                channel = 'retail'
+                nk =  'R-' + rstr.rstr(charset, 4)
+            
+            open_dt = date.today() - timedelta(days=random.randint(0, 2000))
+            # Determine discontinued date and indicator
+            if random.random() < 0.2:  # 20% chance of being discontinued
+                days_since_intro = (date.today() - introduced_dt).days
+                close_dt = open_dt + timedelta(days=random.randint(0, days_since_intro))
+            else:
+                close_dt = '' 
+            f.write(f"{i},{nk},{'Insight ' + fake.city().replace(',',' ')},{channel},{fake.country().replace(',',' ')},{fake.state()},{fake.latitude()},{fake.longitude()},{open_dt}, {close_dt}\n")
+                #Stores
+        suppliers_path = out/'suppliers.csv'
+    with suppliers_path.open('w', encoding='utf-8') as f:
+        f.write('supplier_id,supplier_code,name,country_code,lead_time_days,preffered\n')
+        for i in range(1, 8000):  
+            nk =  'S-' + rstr.rstr(charset, 4)
+            
+            ltd = random.randint(1, 28) # 1 to 28 days
+            # Determine discontinued date and indicator
+            if random.random() < 0.2:  # 20% chance of being discontinued
+                days_since_intro = (date.today() - introduced_dt).days
+                close_dt = open_dt + timedelta(days=random.randint(0, days_since_intro))
+            else:
+                close_dt = '' 
+            f.write(f"{i},{nk},{fake.company().replace(',',' ')},{fake.country_code().replace(',',' ')},{ltd},{fake.boolean()}\n")
     # Shipments parquet sample
     tbl = pa.table({
         'shipment_id': pa.array(range(1, 10001), type=pa.int64()),
