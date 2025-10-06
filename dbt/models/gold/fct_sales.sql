08d5ab7ce207b58639968c45d0d9f9404841bb71
Jack Pan <jack.pan@insight.com>
2025-10-06 17:30:39 +0800

    returns v1, v2 ingestion

--- dbt/models/gold/fct_sales.sql
+++ dbt/models/gold/fct_sales.sql
@@ -11,36 +11,43 @@ SELECT
  --   oh.order_id,
     ol.order_id || '-' || ol.line_number AS sale_id,
     oh.order_ts,
- --   oh.order_dt_local AS order_dt,
+    ol.line_number,
+    oh.order_dt_local AS order_dt,
     oh.customer_id,
     ol.product_id,
+    p.currency as product_currency,
     ol.qty * ol.unit_price AS gross_amount,
-     --                    AS gross_amount_aud,
-     --                    AS total_amount,
-    --                      AS total_amount_aud,
+    ol.qty * ol.unit_price * er.exchange_rate AS gross_amount_aud,
+    ol.line_discount_pct,
+    ol.tax_pct,
+    (ol.qty * ol.unit_price) * (1-COALESCE(ol.line_discount_pct, 0.0))*(1+COALESCE(ol.tax_pct, 0.0)) AS total_amount,
+    (ol.qty * ol.unit_price * er.exchange_rate)*(1-COALESCE(ol.line_discount_pct, 0.0))*(1+COALESCE(ol.tax_pct, 0.0)) AS total_amount_aud,
     ol.qty * ol.unit_price,
     oh.store_id,
     oh.channel,
     oh.payment_method,
     oh.coupon_code,
-    oh.shipping_fee,
-    oh.currency,
-    oh.ingestion_ts AS header_ingestion_ts,
+    oh.shipping_fee as shipping_fee_aud,
+    oh.currency as order_currency,
+--    oh.ingestion_ts AS header_ingestion_ts,
 
     -- From orderslines
-    ol.line_number,
+
 
  --   ol.qty,
  --   ol.unit_price,
-    ol.line_discount_pct,
-    ol.tax_pct,
-    ol.ingestion_ts AS line_ingestion_ts
+
+ --   ol.ingestion_ts AS line_ingestion_ts
 
 FROM {{ ref('silver_orders') }} oh
 JOIN {{ ref('silver_orderslines') }} ol 
- join silver er   oh.currency,
+     ON oh.order_id = ol.order_id
+JOIN {{ ref('dim_product_scd') }} p 
+    on ol.product_id = p.product_id Where p.is_current IS TRUE
+JOIN {{ ref('silver_exchangerates') }} er
+    on oh.order_dt_local= er.date AND p.currency = er.currency
 
-  ON oh.order_id = ol.order_id
+ 
 
 {% if is_incremental() %}
   WHERE ol.ingestion_ts > (
